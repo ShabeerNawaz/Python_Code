9f9e2a5afa6e7246939b7ce5e101bc78cc22efc5
Shabeer Nawaz <127215532+ShabeerNawaz@users.noreply.github.com>
2025-07-09 14:09:05 +0500

    my practice work

---
+++ Q1.py
@@ -0,0 +1,5 @@
+str = input("enter your name:")
+
+print(str.find("b"))
+
+print(str.replace("bee","bbi"))
---
+++ str.py
@@ -0,0 +1,10 @@
+str = "shabeer"
+str1='shani'
+str2="""shah"""
+
+print(str + str1 + str2)
+print(str[2:5])
+print(str2[-4:-1])
+print(str.endswith("er"))
+print(str1.capitalize())
+print(str2.replace("a","aa"))
