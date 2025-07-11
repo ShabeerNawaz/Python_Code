4b385323afb6a176e717aa16a1d164d6eedd6715
Shabeer Nawaz <127215532+ShabeerNawaz@users.noreply.github.com>
2025-07-11 13:02:38 +0500

    my python practice work on listing and tupples

--- List_Tupples.py
+++ List_Tupples.py
@@ -1,4 +1,30 @@
 #list code
 
-marks=[56,32,99,"shah",78.8,65.7,89.4,"shabeer",45.6]
-print(marks)
+list=[56,32,99,78,65,89,45]
+print(list)
+
+#indexing and slicing
+print(list[5])
+
+print(list[2:4])
+
+#List methods
+
+list.append(65)
+print(list)
+
+#sorting
+list1=["shah","qadeer","dhani","rab dino"]
+print(list.sort())
+
+#inserting
+list.insert(4,76)
+print(list)
+
+#removing
+list.remove(76)
+print(list)
+
+#pop
+list.pop(32)
+print(list)
