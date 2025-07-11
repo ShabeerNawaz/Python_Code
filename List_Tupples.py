d9b2e00b4d51644173ab241313b44fa4f2b9abb3
Shabeer Nawaz <127215532+ShabeerNawaz@users.noreply.github.com>
2025-07-11 12:42:01 +0500

    my python practice work on string with conditions,list and tupples

---
+++ List_Tupples.py
@@ -0,0 +1,4 @@
+#list code
+
+marks=[56,32,99,"shah",78.8,65.7,89.4,"shabeer",45.6]
+print(marks)
--- str.py
+++ str.py
@@ -8,3 +8,35 @@ print(str2[-4:-1])
 print(str.endswith("er"))
 print(str1.capitalize())
 print(str2.replace("a","aa"))
+
+#Condition Statements
+
+#first program to check traffic light color
+light = input("Enter traffic light color:")
+if(light=="red"):
+    print("Stop")
+elif(light=="green"):
+    print("Go")
+elif(light=="yellow"):
+    print("wait")
+else:
+    print("Invalid Input")
+print("End of Program")
+
+#grade calculation program
+
+marks=int(input("Enter your Marks"))
+
+if(marks<=100 and marks>0):
+    if(makes >=90):
+        print("Grade A")
+    elif(marks>=80 and marks<90):
+        print("Grade B")
+    elif(marks>=70 and marks<80):
+        print("Grade C")
+    elif(marks >=60 and marks<70):
+        print("Grade D")
+    elif(marks >=50 and marks <60):
+        print("Pass *")
+    else:
+        print("Fail")
